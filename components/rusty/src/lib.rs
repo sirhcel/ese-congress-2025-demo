a8107a71b0e53b4a756fe08a2c0dda426755344c
Christian Meusel <christian.meusel@posteo.de>
2025-12-01 23:37:24 +0100

    Factor panic handler out to runtime module

--- components/rusty/src/lib.rs
+++ components/rusty/src/lib.rs
@@ -1,16 +1,12 @@
 #![no_std]
 
+mod rt;
+
 use core::ffi::c_int;
-use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn rusty_add(left: c_int, right: c_int) -> c_int {
     left + right
-}
-
-#[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
-    loop {}
 }
 
 #[cfg(test)]
