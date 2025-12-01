e997d3d952ab372f2c1e1cef2ca2687bd9841df4
Christian Meusel <christian.meusel@posteo.de>
2025-12-01 23:44:15 +0100

    Add allocator wrapping libc malloc/free

--- components/rusty/src/rt.rs
+++ components/rusty/src/rt.rs
@@ -1,4 +1,10 @@
+extern crate alloc;
+
 use core::panic::PanicInfo;
+use libc_alloc::LibcAlloc;
+
+#[global_allocator]
+static ALLOCATOR: LibcAlloc = LibcAlloc;
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
